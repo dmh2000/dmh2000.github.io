2b350369e712b9077f76dc1e739a472a77e37dd2
david howard <dmh2000@gmail.com>
2018-11-15 12:07:29 -0800

    added PWA support

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+var dataCacheName = 'dmh2000-github.io';
+var cacheName = 'dmh2000-github.io';
+var filesToCache = [
+  '/',
+ "./images",
+ "./images/icons",
+ "./images/icons/icon-128x128.png",
+ "./images/icons/icon-144x144.png",
+ "./images/icons/icon-152x152.png",
+ "./images/icons/icon-192x192.png",
+ "./images/icons/icon-256x256.png",
+ "./index.html",
+ "./manifest.json",
+ "./service-worker.js"
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
